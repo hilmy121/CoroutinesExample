889a38f28d5a64a9968b13ca02614144da22f7c6
Republic Of Gamers <caklunim@gmail.com>
2023-04-30 20:44:26 +0700

    Request call in IO Thread

--- app/src/main/java/com/example/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample/MainActivity.kt
@@ -6,12 +6,17 @@ import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.coroutinesexample.API.DataModel.ImgFlipResponse
+import com.example.coroutinesexample.API.RESTApiObject
 import com.example.coroutinesexample.ViewModel.Adapter.MainAdapter
 import com.example.coroutinesexample.ViewModel.MainViewModel
 import com.example.coroutinesexample.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainViewBinding: ActivityMainBinding
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        testCoroutines()
         getResponseList()
     }
     private fun getResponseList(){
@@ -46,10 +52,59 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+    private fun getCoroutinesList(){
+        GlobalScope.launch(Dispatchers.IO) {
+            RESTApiObject().RESTApiService.getMemes().enqueue(object : Callback<ImgFlipResponse> {
+                override fun onResponse(call: Call<ImgFlipResponse>, response: Response<ImgFlipResponse>) {
+                    response.body().let {
+                        if (it != null) {
+                            Log.i("Your data is processed in", Thread.currentThread().name)
+                            val dataList = it.data?.memes
+                            dataList?.let { memeList ->
+                                Log.i("Your data is displayed in", Thread.currentThread().name)
+                                mainAdapter.setList(memeList)
+                                mainAdapter.notifyDataSetChanged()
+                            }
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<ImgFlipResponse>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
+    }
     private fun setupUI(){
         mainViewBinding.rvMain.setHasFixedSize(true)
         mainViewBinding.rvMain.layoutManager = LinearLayoutManager(this)
         mainViewBinding.rvMain.adapter = mainAdapter
 
+    }
+    private fun testCoroutines(){
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.i("Processed in : ",Thread.currentThread().name)
+
+        }
+        RESTApiObject().RESTApiService.getMemes().enqueue(object: Callback<ImgFlipResponse>{
+            override fun onResponse(call: Call<ImgFlipResponse>, response: Response<ImgFlipResponse>) {
+                GlobalScope.launch(Dispatchers.IO){
+                    response.body().let {
+                            body ->
+                        if (body != null) {
+                            Log.i("Data is requested in : ",Thread.currentThread().name)
+                        }
+                    }
+                }
+
+            }
+
+            override fun onFailure(call: Call<ImgFlipResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        Log.i("Now Processed in : ",Thread.currentThread().name)
     }
 }
